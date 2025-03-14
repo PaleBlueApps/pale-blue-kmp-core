bd94b436c714927fd6fcc7ee7f9fe0462998aec9
Anil Kumar Beesetti <abeesetti@paleblueapps.com>
2025-03-14 16:33:32 +0530

    add maven publish plugin and configure it

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("maven-publish")
 }
 
 kotlin {
@@ -14,6 +15,7 @@ kotlin {
                 }
             }
         }
+        publishLibraryVariants("release")
     }
 
     iosX64()
@@ -56,5 +58,40 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+publishing {
+    publications.withType<MavenPublication> {
+        groupId = "com.paleblueapps"
+        artifactId = "kmmcore"
+        version = "1.0"
+
+        pom {
+            name.set("PaleBlueKmmCore")
+            description.set("A Kotlin Multiplatform library")
+            url.set("https://github.com/paleblueapps/pale-blue-kmm-core")
+
+            licenses {
+//                license {
+//                    name.set("The Apache Software License, Version 2.0")
+//                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                    distribution.set("repo")
+//                }
+            }
+            developers {
+                developer {
+                    id.set("paleblueapps")
+                    name.set("Pale Blue Apps")
+                }
+            }
+            scm {
+                url.set("https://github.com/paleblueapps/pale-blue-kmm-core")
+            }
+        }
+
+        repositories {
+            mavenLocal()
+        }
     }
 }
